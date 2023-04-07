961353600dec5f0437954f378b0fc3ad6b9b42e7
Shimi Ben Kalifa <37695804+shimiBK@users.noreply.github.com>
2023-04-07 22:45:00 +0300

    Update before_install.sh

--- client/scripts/before_install.sh
+++ client/scripts/before_install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/server
+sudo apt-get update
+sudo apt-get install curl
 curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
