ebd983e8af3b0ec943729654949d7d7ce51012b2
Shimi Ben Kalifa <37695804+shimiBK@users.noreply.github.com>
2023-04-08 08:57:37 +0300

    Update app_start.sh

--- client/scripts/app_start.sh
+++ client/scripts/app_start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-cd /home/ec2-user/server/src
-npm start
-pm2 start npm --name "ridedm" -- start
-pm2 startup
-pm2 save
-pm2 restart all
+cd /var/www/html/RIDEDM/client
+pm2 start npm -- start
