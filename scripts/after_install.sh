c2f29ac9fff12633f0b7adcc24604221c3cbe138
Shimi Ben Kalifa <37695804+shimiBK@users.noreply.github.com>
2023-04-17 18:08:53 +0300

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cd /var/www/html/RIDEDM/client
-sudo npm install
-sudo npm install react-scripts
-sudo npm install pm2 -g
+npm install
+npm install react-scripts
+npm install pm2 -g
 cd ../api
 sudo rm -rf node_modules
-sudo npm install
-sudo npm install forever -g
+npm install
+npm install forever -g
 cd ../socket
 sudo rm -rf node_modules
-sudo npm install
-sudo npm install forever -g
+npm install
+npm install forever -g
