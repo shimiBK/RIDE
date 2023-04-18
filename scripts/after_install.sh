f94490a52cf614860112281465a15f48183b96d5
Shimmy BK <shimi.sbk@gmail.com>
2023-04-18 12:39:32 +0300

    [update] update version to react-scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,10 +3,10 @@ cd /var/www/html/RIDEDM/client
 npm install
 npm install pm2 -g
 cd ../api
-sudo rm -rf node_modules
+sudo rm -r node_modules
 npm install
 npm install forever -g
 cd ../socket
-sudo rm -rf node_modules
+sudo rm -r node_modules
 npm install
 npm install forever -g
