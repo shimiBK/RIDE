f38164b8f9331b383f1e026db2e4500cccd7d8df
Shimi Ben Kalifa <37695804+shimiBK@users.noreply.github.com>
2023-04-19 22:33:44 +0300

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,11 +3,4 @@ cd /home/ec2-user/server/client
 npm install
 npm install --save react-scripts
 npm install pm2 -g
-cd ../api
-sudo rm -r node_modules
-npm install
-npm install forever -g
-cd ../socket
-sudo rm -r node_modules
-npm install
-npm install forever -g
+
