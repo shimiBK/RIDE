e02711997cfad6b1a2c3d1e53a3c428119223964
Shimi Ben Kalifa <37695804+shimiBK@users.noreply.github.com>
2023-04-19 22:33:57 +0300

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -5,7 +5,3 @@ pm2 start npm --name "covidapp" -- start
 pm2 startup
 pm2 save
 pm2 restart all
-cd ../api
-forever start --minUptime 1000 --spinSleepTime 1000 index.js
-cd ../socket
-forever start --minUptime 1000 --spinSleepTime 1000 index.js
