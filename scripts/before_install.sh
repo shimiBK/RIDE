10a774863f1973bc3df1a781ab4f6cc954ea8b52
Shimi Ben Kalifa <37695804+shimiBK@users.noreply.github.com>
2023-04-21 02:35:05 +0300

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,5 +2,5 @@
 cd /home/ec2-user/server
 sudo yum update
 sudo yum install curl
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo yum install -y nodejs
